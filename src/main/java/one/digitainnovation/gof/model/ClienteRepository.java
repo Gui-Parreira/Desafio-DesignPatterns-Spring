ba8fc7943b39d0a0b50862d4a28e9140d01b7f57
Gui-Parreira <gui.parreira98@gmail.com>
2024-06-25 14:21:31 -0300

    Implementando Design pattern com Java Spring (GOF)

---
+++ src/main/java/one/digitainnovation/gof/model/ClienteRepository.java
@@ -0,0 +1,6 @@
+package one.digitainnovation.gof.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+}
---
+++ src/main/java/one/digitainnovation/gof/service/ClienteService.java
@@ -0,0 +1,11 @@
+package one.digitainnovation.gof.service;
+
+import one.digitainnovation.gof.model.Cliente;
+
+public interface ClienteService {
+    Iterable<Cliente> buscarTodos();
+    Cliente buscarPorId(Long id);
+    void inserir(Cliente cliente);
+    void atualizar(Long id, Cliente cliente);
+    void deletar(Long id);
+}
